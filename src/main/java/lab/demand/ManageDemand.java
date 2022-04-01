61f21e53b7325152e1047db2c113c9e7e01961d8
christopherwilliamflores <christopher.flores.ri@gmail.com>
2022-03-31 21:48:46 -0500

    change 1

--- src/main/java/lab/demand/ManageDemand.java
+++ src/main/java/lab/demand/ManageDemand.java
@@ -1,5 +1,4 @@
 package lab.demand;
-
 import java.util.List;
 
 public class ManageDemand {
--- src/test/java/lab/TaxTest.java
+++ src/test/java/lab/TaxTest.java
@@ -1,10 +1,18 @@
 package lab;
 
+import lab.demand.Order;
+import lab.util.TestUtil;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+@Test
 public class TaxTest {
 
-    // Add Tests !
-    
+    public void test_AllTaxFromPeru() {
+        List<Order> ordersFromPeru = TestUtil.buildOrdersPeru();
+        double result = demand.calculateTotal(ordersFromPeru);
+        Assert.assertEquals(Math.round(result), 7.0);
+    }
 }
